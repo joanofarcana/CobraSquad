d0df1b670be1634ec058b995d55c1ef609475682
Arielle Evans <ariellelevans@gmail.com>
2015-11-07 21:59:08 -0500

    more fixes

--- 249assignment2/src/BookInventory1.java
+++ 249assignment2/src/BookInventory1.java
@@ -7,7 +7,7 @@
 //				Himmet Arican		27533934
 // For COMP249 Section: (Substitute your section letter(s))
 // ---------------------------------------------------------------
-package book;
+package src;
 
 import java.util.Scanner;
 import java.io.*;
@@ -35,44 +35,43 @@ public class BookInventory1 {
 			} catch (Exception ex) {
 				System.out.println("A file with that name already exists");
 				try {
-					BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filename), "utf-8"));
+					File f = new File(filename);
 					//need to fix this. supposed to show the size of the already-existing file
-					System.out.println("The size of that file is ");
-					br.close();
+					System.out.println("The size of that file is " + f.length());
 				} catch (Exception ex1) {
+					//this really shouldn't happen...
 				}
 				System.out.print("Please enter a name for a new text file ");
 				filename = scan.next();
 			}
 		} while (fileexists);
-
-
-        try{
-		    fixInventory("Initial_Book_Info.txt", filename);
-		}catch(Exception ex5){
-		    System.out.println("Could not fix inventory");
+		try {
+			fixInventory("Initial_Book_Info.txt", filename);
+		} catch (Exception ex5) {
+			System.out.println("Could not fix inventory");
+			ex5.printStackTrace();
 		}
 	}
 
 	public static int countBooks(String filename) {
 		int lines = 0;
 		try {
-		    File file = new File(filename);
+			File file = new File(filename);
 			Scanner temp = new Scanner(file);
-			while (temp.hasNextLine()){
-			    lines++;
-			    temp.nextLine();
+			while (temp.hasNextLine()) {
+				lines++;
+				temp.nextLine();
 			}
 			temp.close();
 		} catch (Exception e) {
 			return 0;
 		}
-		return lines;
+		return lines-1;
 	}
 
 	public static boolean checkDuplicate(Book[] array, long isbn, int index) {
-		for (int i = 0; i < index; i++) {
-			if ((array[i].getISBN() == isbn)&&(i!=index)) {
+		for (int i = 0; i < array.length; i++) {
+			if ((array[i].getISBN() == isbn) && (i != index)) {
 				return true;
 			}
 		}
@@ -81,7 +80,7 @@ public class BookInventory1 {
 
 
 
-	public static void fixInventory(String inname, String outname) throws Exception{
+	public static void fixInventory(String inname, String outname) throws Exception {
 
 		Scanner input = new Scanner(System. in );
 		//number of books in the file
@@ -97,38 +96,47 @@ public class BookInventory1 {
 		String tempauthor;
 		double tempprice;
 		int tempnum;
-		
+
 		//file with books in it
 		File file1 = new File(inname);
 		Scanner scan = new Scanner(file1);
-		
+
 		//creating objects in bkArr
 		for (int i = 0; i < records; i++) {
-            if(scan.hasNextLine()){
-			    tempisbn = Long.parseLong(scan.next(), 10);
-		        temptitle = scan.next();
-			    tempyear = Integer.parseInt(scan.next());
-			    tempauthor = scan.next();
-			    tempprice = Double.parseDouble(scan.next());
-			    tempnum = Integer.parseInt(scan.next());
-			    bkArr[i] = new Book(tempisbn, tempyear, temptitle, tempauthor, tempprice, tempnum);
+			if (scan.hasNextLine()) {
+			    System.out.println(""+records);
+				tempisbn = Long.parseLong(scan.next(), 10);
+				temptitle = scan.next();
+				tempyear = Integer.parseInt(scan.next());
+				tempauthor = scan.next();
+				tempprice = Double.parseDouble(scan.next());
+				tempnum = Integer.parseInt(scan.next());
+				bkArr[i] = new Book(tempisbn, tempyear, temptitle, tempauthor, tempprice, tempnum);
 			}
 		}
-        
-        //checking for duplicate isbns
+
+		//checking for duplicate isbns
 		for (int j = 0; j < records; j++) {
 			long isbn = bkArr[j].getISBN();
 			for (int k = j + 1; k < records; k++) {
 				if (bkArr[k].getISBN() == isbn) {
 					do {
-					    System.out.println("ISBN error found. Please enter a new isbn for "+bkArr[k].getTitle()+" "+bkArr[k].getISBN()+": ");
+						System.out.println("ISBN error found. Please enter a new isbn for " + bkArr[k].getTitle() + " " + bkArr[k].getISBN() + ": ");
 						bkArr[k].setISBN(input.nextLong());
 					} while (checkDuplicate(bkArr, bkArr[k].getISBN(), k));
 				}
 			}
 		}
+		
+		Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outname), "utf-8"));
+		for (int i = 0; i < records; i++) {
+			writer.write("" + bkArr[i].getISBN() + " " + bkArr[i].getIssueYear() + 
+			            " " + bkArr[i].getTitle() + " " + bkArr[i].getAuthor() + 
+			            " " + bkArr[i].getPrice() + " " + bkArr[i].getPages() + "\n");
+		}
+		writer.close();
 	}
-	
+
 	public static void displayFileContents(String inname) {
 
 	}
